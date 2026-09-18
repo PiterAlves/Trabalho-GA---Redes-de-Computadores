442285c738e620398164a589b2e7f264c7e2e808
Piter Alves <alves_piter@hotmail.com>
2026-09-17 23:00:25 -0300

    ajuste no print

--- nodo.py
+++ nodo.py
@@ -135,8 +135,8 @@ class PeerNode:
                     print(f"[*] Nodo novo se atualizando: solicitando '{arq}' de {addr}")
                     self.enviar_mensagem({"tipo": "PEDIR", "nome": arq}, addr[0], addr[1])
                     arquivos_baixados += 1
-                if arquivos_baixados == 0:
-                    print("Verificação concluida: A pasta já está sincronizada com os peers conhecidos.")
+            if arquivos_baixados == 0:
+                print("Verificação concluida: A pasta já está sincronizada com os peers conhecidos.")
 
     def enviar_arquivo_fatiado(self, nome, addr):
         # Lê o arquivo local, divide em blocos de 1KB e envia em datagramas UDP com base64
